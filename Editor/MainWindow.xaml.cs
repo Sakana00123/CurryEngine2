adb4fd8f68f949c8834f70cd031640efd62a163d
Sakana00123 <dheitian5@gmail.com>
2025-10-29 18:33:09 +0900

    GameEngineProgramming001途中（70%くらい）

---
+++ Editor/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Editor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowserDialog = new ProjectBrowserDialog();
+            
+            if (projectBrowserDialog.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                // Load the selected project
+            }
+        }
+    }
+}
