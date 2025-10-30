8e3ff095e3c5c011b8be8e5a4a50bc09ef233376
Sakana00123 <dheitian5@gmail.com>
2025-10-30 21:57:44 +0900

    GameEngineProgramming002途中

--- Editor/GameProject/NewProject.cs
+++ Editor/GameProject/NewProject.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Editor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Editor.GameProject
 {
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+    }
+
     class NewProject : ViewModelBase
     {
+        // TODO: get the path from the installation location
+        private readonly string _templatePath = @"..\..\CurryEditor\ProjectTemplates";
         private string _name = "NewProject";
         public string Name
         {
@@ -31,6 +48,31 @@ namespace Editor.GameProject
                     _path = value;
                     OnPropertyChanged(nameof(Path));
                 }
+            }
+        }
+
+        public NewProject()
+        {
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach (var file in templatesFiles)
+                {
+                    var template = new ProjectTemplate()
+                    {
+                        ProjectType = "Empty Project",
+                        ProjectFile = "Project.Curry",
+                        Folders = new List<string>() { ".Curry", "Assets", "Scripts", "Scenes" }
+                    };
+
+                    Serializer.ToFile(template, file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: log error
             }
         }
     }
--- Editor/obj/Debug/Editor.AssemblyInfo.cs
+++ Editor/obj/Debug/Editor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1060bcad04f9e374e9e9ec02b4ee605238563015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602728328154a3313fcd6d23a26d32a80a5fa80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
