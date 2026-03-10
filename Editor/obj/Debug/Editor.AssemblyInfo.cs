a2bcf26922f3298dfc305277b25a0145efba31ac
Sakana00123 <dheitian5@gmail.com>
2026-03-10 18:58:04 +0900

    途中でいったん置いてる。TransformViewを出しただけ。

--- Editor/obj/Debug/Editor.AssemblyInfo.cs
+++ Editor/obj/Debug/Editor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb609b51bcfe7234ff686e537d2466b567d5dde1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39618b36c9310795ca8d061c12ab92713326e0fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
